78791dee7aef2830bc2f52bdd41225648d9d4515
Tiago Malaquias de Souza Oliveira <108484845+souzz2@users.noreply.github.com>
2025-03-29 11:16:25 -0300

    Criando a organização de pastas

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,22 @@ const app = express();
 const PORT = 3000;
 mongoose.connect('mongodb+srv://organizei:nfameCN5lac0il4w@organizei-api.px4oxxo.mongodb.net/?retryWrites=true&w=majority&appName=organizei-api');
 
+const User = mongoose.model('User',{ name: String, email: String, password: String });
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("API rodando com Node.js e TypeScript 🚀");
+});
+
+app.post("/", async (req: Request, res: Response) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    await user.save();
+    res.send(user);
 });
 
 app.listen(PORT, () => {
