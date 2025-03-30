6dc34c62d0c351722e6c7cb900269158d1632824
Tiago Malaquias de Souza Oliveira <108484845+souzz2@users.noreply.github.com>
2025-03-30 19:55:53 -0300

    apaguei tudo

--- src/router/userRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { UserController } from "../controller/userController";
-
-const router = express.Router();
-const userController = new UserController();
-
-router.post("/register", userController.createUser);
-
-export default router;
