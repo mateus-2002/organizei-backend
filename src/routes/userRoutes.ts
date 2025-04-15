79b757495a02dbb62018710e4b727e7a56e1e7fb
souzz2 <108484845+souzz2@users.noreply.github.com>
2025-04-15 12:51:54 -0300

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,8 +4,10 @@ import { UserController } from '../controllers/UserController';
 const router = Router();
 const userController = new UserController();
 
-router.get('/users', userController.getUsers as RequestHandler);
+router.put('/users/:id', userController.editUser as RequestHandler);
 router.post('/login', userController.login as RequestHandler);
 router.post('/signup', userController.signup as RequestHandler);
+router.get('/users/:id', userController.getUserById as RequestHandler);
+router.get('/users', userController.getUsers as RequestHandler);
 
 export default router;
